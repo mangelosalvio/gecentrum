7132097694be4e25d751bb673607badd5081db55
mangelosalvio <msalvio.13@gmail.com>
2016-06-26 15:38:30 +0800

    Fixed History Issues

--- resources/views/reports/delivery_receipts/print_delivery_receipts_history.blade.php
+++ resources/views/reports/delivery_receipts/print_delivery_receipts_history.blade.php
@@ -25,17 +25,8 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <?
-                            $arr_total = array();
-                            $arr_total['amount'] = 0;
-                            $arr_total['quantity'] = 0;
-                        ?>
                         @foreach($DRs as $DR)
                             @foreach($DR->details as $Detail)
-                                <?
-                                        $arr_total['amount'] += $Detail->amount;
-                                        $arr_total['quantity'] += $Detail->quantity;
-                                ?>
                                 <tr>
                                     <td>{{ $DR->date }}</td>
                                     <td>{{ str_pad($DR->id,7,0,STR_PAD_LEFT) }}</td>
--- resources/views/reports/rr/print_rr_history.blade.php
+++ resources/views/reports/rr/print_rr_history.blade.php
@@ -26,17 +26,8 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <?
-                            $arr_total = array();
-                            $arr_total['amount'] = 0;
-                            $arr_total['quantity'] = 0;
-                        ?>
                         @foreach($RRs as $RR)
                             @foreach($RR->details as $Detail)
-                                <?
-                                        $arr_total['amount'] += $Detail->amount;
-                                        $arr_total['quantity'] += $Detail->quantity;
-                                ?>
                                 <tr>
                                     <td>{{ $RR->date }}</td>
                                     <td>{{ str_pad($RR->id,7,0,STR_PAD_LEFT) }}</td>
